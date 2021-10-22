efdaedfc857c2bbb3c3cae8e032ea35e5a91f4dc
Chris Williamson <chris.williamson@nesta.org.uk>
2021-10-22 09:53:52 +0100

    formatting tweaks

--- heat_pump_adoption_modelling/analysis/plot_mcs.py
+++ heat_pump_adoption_modelling/analysis/plot_mcs.py
@@ -6,90 +6,77 @@ import matplotlib.pyplot as plt
 from heat_pump_adoption_modelling import PROJECT_DIR
 from heat_pump_adoption_modelling.pipeline.load_mcs import load_domestic_hps
 
+
 def plottable_dhps():
     dhps = load_domestic_hps()
     dhps["year"] = dhps.date.dt.year
     dhps_noexhaust = dhps[dhps.tech_type != "Exhaust Air Heat Pump"]
-    
+
     return dhps_noexhaust
+
 
 # median installation cost per year
 def plot_median_costs(df):
-    
+
     cost_data = (
-        df
-        .groupby(["year", "tech_type"])
-        ["cost"]
-        .median()
-        .loc[2015:2021]
-        .unstack()
+        df.groupby(["year", "tech_type"])["cost"].median().loc[2015:2021].unstack()
     )
 
     fig, ax = plt.subplots()
-    
+
     ax.plot(
         cost_data.index,
-        cost_data['Ground/Water Source Heat Pump'].tolist(),
-        label = 'Ground/Water Source Heat Pump',
-        c = 'blue'
+        cost_data["Ground/Water Source Heat Pump"].tolist(),
+        label="Ground/Water Source Heat Pump",
+        c="blue",
     )
     ax.plot(
         cost_data.index,
-        cost_data['Air Source Heat Pump'].tolist(),
-        label = 'Air Source Heat Pump',
-        c = 'orange'
+        cost_data["Air Source Heat Pump"].tolist(),
+        label="Air Source Heat Pump",
+        c="orange",
     )
     ax.set_ylim(0, 21000)
-    ax.grid(axis='y', color='0.8')
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Median cost of installation (£)')
-    ax.legend(title = "Heat pump type")
-    ax.set_title('Median cost of heat pump installations\n(years with >100 installations only)')
+    ax.grid(axis="y", color="0.8")
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Median cost of installation (£)")
+    ax.legend(title="Heat pump type")
+    ax.set_title(
+        "Median cost of heat pump installations\n(years with >100 installations only)"
+    )
 
-    plt.savefig(PROJECT_DIR / 'outputs/figures/median_costs.png')
+    plt.savefig(PROJECT_DIR / "outputs/figures/median_costs.png")
 
 
 # numbers of installations each year
 def plot_installation_numbers(df):
-    
+
     numbers_data = (
-        df
-        .groupby(["year", "tech_type"])
-        ["cost"]
-        .count()
-        .unstack()
-        .loc[2014:2021]
+        df.groupby(["year", "tech_type"])["cost"].count().unstack().loc[2014:2021]
     )
 
     fig, ax = plt.subplots()
-    
-    numbers_data.plot(
-        kind="bar",
-        ax=ax,
-        zorder=3,
-        color=['orange', 'blue']
-    )
-    ax.set_xlabel('Year')
-    ax.set_ylabel('Installations')
-    ax.grid(axis='y', color='0.8', zorder=0)
-    ax.legend(title = 'Heat pump type')
-    ax.set_title('Numbers of heat pumps installed')
+
+    numbers_data.plot(kind="bar", ax=ax, zorder=3, color=["orange", "blue"])
+    ax.set_xlabel("Year")
+    ax.set_ylabel("Installations")
+    ax.grid(axis="y", color="0.8", zorder=0)
+    ax.legend(title="Heat pump type")
+    ax.set_title("Numbers of heat pumps installed")
     plt.xticks(rotation=0)
     plt.tight_layout()
-    
-    plt.savefig(PROJECT_DIR / 'outputs/figures/median_costs.png')
 
-
-
+    plt.savefig(PROJECT_DIR / "outputs/figures/median_costs.png")
 
 
 #### Geographic distribution - local authorities
 def regional_plot(df):
-    df.groupby('local_authority').agg(
-            number = ('cost', lambda col: col.count()), 
-            median_cost = ('cost', lambda col: col.median())
-        ).sort_values('median_cost', ascending=False)
+    df.groupby("local_authority").agg(
+        number=("cost", lambda col: col.count()),
+        median_cost=("cost", lambda col: col.median()),
+    ).sort_values("median_cost", ascending=False)
     # tbc...
+
 
 # some LAs with not many installations
 # some non-standard local authority names -
--- heat_pump_adoption_modelling/pipeline/mcs_epc_joining.py
+++ heat_pump_adoption_modelling/pipeline/mcs_epc_joining.py
@@ -8,13 +8,20 @@ import time
 from heat_pump_adoption_modelling import PROJECT_DIR
 from heat_pump_adoption_modelling.pipeline.load_mcs import load_domestic_hps
 
-epc_address_fields = ['ADDRESS1', 'POSTTOWN', 'POSTCODE']
-epc_characteristic_fields = ['TOTAL_FLOOR_AREA', 'CONSTRUCTION_AGE_BAND', 'BUILT_FORM', 'PROPERTY_TYPE', 'HP_INSTALLED']
+epc_address_fields = ["ADDRESS1", "POSTTOWN", "POSTCODE"]
+epc_characteristic_fields = [
+    "TOTAL_FLOOR_AREA",
+    "CONSTRUCTION_AGE_BAND",
+    "BUILT_FORM",
+    "PROPERTY_TYPE",
+    "HP_INSTALLED",
+]
 matching_parameter = 0.7
 # TODO: put this in config
 
 
 #### UTILS
+
 
 def rm_punct(address):
     if (address is pd.NA) | (address is np.nan):
@@ -24,24 +31,25 @@ def rm_punct(address):
         address = address.replace("/", "_").replace("-", "_")
         # remove all punctuation other than _
         address = address.translate(
-            str.maketrans('', '', string.punctuation.replace("_", ""))
+            str.maketrans("", "", string.punctuation.replace("_", ""))
         )
-        
+
         return address
+
 
 # \w below interprets _ as part of a word
 # /s and -s are important to keep in things like "Flat 3/2" and "Flat 3-2"
 # not sure if this breaks address similarity when it appears in words though
 
+
 def extract_numeric_tokens(df):
     # Extracts character strings containing numbers
     # as these are likely to be house or building numbers
     token_list = [
-        set(re.findall('\w*\d\w*', address))
-        for address
-        in df['standardised_address']
+        set(re.findall("\w*\d\w*", address)) for address in df["standardised_address"]
     ]
     return token_list
+
 
 # this isn't perfect - e.g. "Flat 3, 3 Street Name" would
 # just become {3} - could use a multiset
@@ -54,52 +62,45 @@ def extract_numeric_tokens(df):
 
 #### PREPROCESSING
 
+
 def prepare_dhps():
     dhps = load_domestic_hps()
-    
-    dhps['standardised_postcode'] = [
-        pc.upper().strip()
-        for pc
-        in dhps['postcode'].fillna("unknown")
+
+    dhps["standardised_postcode"] = [
+        pc.upper().strip() for pc in dhps["postcode"].fillna("unknown")
     ]
-    
-    dhps['standardised_address'] = [
+
+    dhps["standardised_address"] = [
         rm_punct(add1).lower() + " " + rm_punct(add2).lower()
-        for add1, add2
-        in zip(dhps.address_1.fillna(""), dhps.address_2.fillna(""))
+        for add1, add2 in zip(dhps.address_1.fillna(""), dhps.address_2.fillna(""))
     ]
-    
-    dhps['numeric_tokens'] = extract_numeric_tokens(dhps)
-    
+
+    dhps["numeric_tokens"] = extract_numeric_tokens(dhps)
+
     return dhps
 
 
 def prepare_epcs():
-    epcs = (
-        pd.read_csv(
-            PROJECT_DIR / 'outputs/EPC_data/preprocessed_data/Q2_2021/EPC_GB_preprocessed_and_deduplicated.csv',
-            usecols = epc_address_fields + epc_characteristic_fields
-        )
+    epcs = pd.read_csv(
+        PROJECT_DIR
+        / "outputs/EPC_data/preprocessed_data/Q2_2021/EPC_GB_preprocessed_and_deduplicated.csv",
+        usecols=epc_address_fields + epc_characteristic_fields,
     )
-    
+
     # Remove spaces, uppercase and strip whitespace from
     # postcodes in order to exact match on this field
-    epcs['standardised_postcode'] = [
-        postcode.replace(" ", "")
-        for postcode
-        in epcs['POSTCODE'].fillna('UNKNOWN')
+    epcs["standardised_postcode"] = [
+        postcode.replace(" ", "") for postcode in epcs["POSTCODE"].fillna("UNKNOWN")
     ]
-    
+
     # Remove punctuation, lowercase and concatenate address fields
     # for approximate matching
-    epcs['standardised_address'] = [
-        rm_punct(address).lower()
-        for address
-        in epcs['ADDRESS1']
+    epcs["standardised_address"] = [
+        rm_punct(address).lower() for address in epcs["ADDRESS1"]
     ]
-    
-    epcs['numeric_tokens'] = extract_numeric_tokens(epcs)
-    
+
+    epcs["numeric_tokens"] = extract_numeric_tokens(epcs)
+
     return epcs
 
 
@@ -108,75 +109,72 @@ def prepare_epcs():
 
 #### JOINING
 
+
 def form_matching(df1, df2):
-    
+
     # Index
-    print('- Forming an index...')
+    print("- Forming an index...")
     indexer = rl.Index()
-    indexer.block(on='standardised_postcode')
+    indexer.block(on="standardised_postcode")
     candidate_pairs = indexer.index(df1, df2)
-    
+
     # Compare
-    print('- Forming a comparison...')
+    print("- Forming a comparison...")
     comp = rl.Compare()
-    comp.exact(
-        'numeric_tokens',
-        'numeric_tokens',
-        label='numerics'
-    )
+    comp.exact("numeric_tokens", "numeric_tokens", label="numerics")
     comp.string(
-        'standardised_address',
-        'standardised_address',
-        method='jarowinkler',
-        label='address'
+        "standardised_address",
+        "standardised_address",
+        method="jarowinkler",
+        label="address",
     )
-    
+
     # Classify
-    print('- Computing a matching...')
+    print("- Computing a matching...")
     matching = comp.compute(candidate_pairs, df1, df2)
-    
+
     return matching
 
 
 def join_mcs_epc_data(dhps=None, epcs=None, save=True):
-    
-    print('Preparing domestic HP data...')
+
+    print("Preparing domestic HP data...")
     if dhps is None:
         dhps = prepare_dhps()
-    
-    print('Preparing EPC data...')
+
+    print("Preparing EPC data...")
     if epcs is None:
         epcs = prepare_epcs()
-    
-    print('Forming a matching...')
+
+    print("Forming a matching...")
     matching = form_matching(df1=dhps, df2=epcs)
-    
+
     good_matches = matching[
         (matching.numerics == 1) & (matching.address >= matching_parameter)
     ]
-    
+
     def epc_lookup(i):
         try:
             matches = good_matches.loc[i]
-            index = matches.idxmax()['address']
-            return epcs.loc[index][
-                epc_characteristic_fields + ['standardised_address']
-            ]
+            index = matches.idxmax()["address"]
+            return epcs.loc[index][epc_characteristic_fields + ["standardised_address"]]
         except KeyError:
             return np.nan
-    
-    dhps[epc_characteristic_fields + ['standardised_address']] = np.nan
-    
-    print('Joining the data...')
+
+    dhps[epc_characteristic_fields + ["standardised_address"]] = np.nan
+
+    print("Joining the data...")
     # this bit is really slow - is there a quicker method?
     for i in dhps.index:
-        dhps.loc[i, epc_characteristic_fields + ['standardised_address']] = epc_lookup(i)
-    
-    merged = dhps.drop(columns=['standardised_postcode', 'numeric_tokens'])
-    
+        dhps.loc[i, epc_characteristic_fields + ["standardised_address"]] = epc_lookup(
+            i
+        )
+
+    merged = dhps.drop(columns=["standardised_postcode", "numeric_tokens"])
+
     if save:
-        merged.to_csv(PROJECT_DIR / 'outputs/mcs_epc.csv')
-    
+        merged.to_csv(PROJECT_DIR / "outputs/mcs_epc.csv")
+
     return merged
 
 
@@ -193,12 +191,9 @@ def main():
     end_time = time.time()
     runtime = round((end_time - start_time) / 60)
 
-    print("\nLoading and joining MCS and EPC data took {} minutes.".format(runtime))
+    print("Loading and joining MCS and EPC data took {} minutes.".format(runtime))
 
 
 if __name__ == "__main__":
     # Execute only if run as a script
     main()
-
-
-
