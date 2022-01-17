a929617c867e0126056525950e56caafd5a93cd1
Julia Suter <juliasuter@NESTA-FVFF81W0Q05P.local>
2022-01-17 13:37:46 +0100

    Hyperparameter screening

--- heat_pump_adoption_modelling/pipeline/supervised_model/utils/hyperparameter_screening.py
+++ heat_pump_adoption_modelling/pipeline/supervised_model/utils/hyperparameter_screening.py
@@ -1,4 +1,45 @@
+# File: heat_pump_adoption_modelling/pipeline/supervised_model/utils/hyperparameter_screening.py
+"""
+Hyperparameter screening for supervised models.
+"""
+
+# ----------------------------------------------------------------------------------
+
+
 from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn import svm
+
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+
+import numpy as np
+
+prepr_pipeline = Pipeline(
+    [
+        ("imputer", SimpleImputer(missing_values=np.nan, strategy="median")),
+        ("min_max_scaler", MinMaxScaler()),
+        ("pca", PCA(n_components=0.9, random_state=42)),
+    ]
+)
+
+model_dict = {
+    "Random Forest Regressor": RandomForestRegressor(),
+    #  "SVM Regressor": svm.SVR(),
+    #  "Linear Regression": LinearRegression(),
+    #   "Decision Tree Regressor": DecisionTreeRegressor(),
+    "Logistic Regression": LogisticRegression(random_state=42),
+    "Linear Support Vector Classifier": SGDClassifier(random_state=42),
+    "Support Vector Classifier": svm.SVC(probability=True, random_state=42),
+}
+
 
 param_grid_dict = {
     "Linear Support Vector Classifier": {
@@ -8,11 +49,12 @@ param_grid_dict = {
         "tol": [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.01, 0.1],
     },
     "Random Forest Regressor": {
-        "n_estimators": [10, 15, 20, 25],
+        "n_estimators": [10, 15, 18, 20, 23],
         "max_features": [5, 10, 15, 20, 25, "auto", "sqrt"],
         "min_samples_leaf": [0.05],
         "bootstrap": [False],
     },  # {'bootstrap': False, 'max_features': 10, 'min_samples_leaf': 0.05, 'n_estimators': 20}
+    # {'bootstrap': False, 'max_features': 15, 'min_samples_leaf': 0.05, 'n_estimators': 15}
     "SVM Regressor": {
         "gamma": [0.0001, 0.001, 0.01, 0.1, 1.0, 10],
         "kernel": ["rbf"],
@@ -31,15 +73,12 @@ param_grid_dict = {
 }
 
 
-def hyperparameter_screening(model, model_name, X, y, scoring):
+def grid_screening(model_name, X, y, scoring):
     """Screen hyper parameters for supervised learning models
     and print out best combiantion.
 
     Parameters
     ----------
-    model : sklearn.model
-        Model for which to screen hyperparameters.
-
     model_name : str
         Supervised learning model to use.
 
@@ -56,13 +95,22 @@ def hyperparameter_screening(model, model_name, X, y, scoring):
     Return: None
     """
 
+    for feat in X.columns:
+        if feat.startswith("POSTCODE"):
+            del X[feat]
+
+    X_prep = prepr_pipeline.fit_transform(X)
+
+    print(X_prep.shape)
+
     # Get model and parameter dictionary
+    model = model_dict[model_name]
     param_grid = param_grid_dict[model_name]
 
     # Apply grid search for finding the best parameters
     grid_search = GridSearchCV(model, param_grid, cv=3, scoring=scoring)
 
     # Fit the model and find best parameters
-    grid_search.fit(X, y)
+    grid_search.fit(X_prep, y)
     print(model_name)
     print(grid_search.best_params_)
