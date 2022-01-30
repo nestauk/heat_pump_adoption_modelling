56a15924cebea2db248ffb25201496719948ec0a
Julia Suter <juliasuter@NESTA-FVFF81W0Q05P.local>
2022-01-30 20:47:40 +0000

    More consistent naming and commenting

--- heat_pump_adoption_modelling/pipeline/supervised_model/data_aggregation.py
+++ heat_pump_adoption_modelling/pipeline/supervised_model/data_aggregation.py
@@ -56,7 +56,7 @@ def get_postcode_levels(df, only_keep=None):
     df: pandas.DataFrame
         Dataframe with column "POSTCODE"
 
-    only_keep: str, efault=None
+    only_keep: str, default=None
         If postcode level is given, the other postcode levels will be removed.
         For instance, if "POSTCODE_DISTRICT" is given, remove
         columns "POSTCODE_ADEA/SECTOR/UNIT" from dataframe.
@@ -79,8 +79,16 @@ def get_postcode_levels(df, only_keep=None):
     return df
 
 
-def aggreate_categorical_features(df, features, agglo_feature="POSTCODE_UNIT"):
+def aggregate_categorical_features(df, features, group_by_feature="POSTCODE_UNIT"):
     """Aggregate categorical features.
+
+    First split the data into groups, e.g. based on postcode level, and get the number of samples.
+    For each categorial feature, get the percentage of each category/value for every group.
+    Also retrieve the most frequent category for every feature.
+
+    For example: Postcode AB10 1QS has 8 samples. Feature TENURE is split in the following categories:
+    12.5% private rental, 37.5% social rental and 50% owner-occupied properties.
+    Thus, the most freuquent category for TENURE is owner-occupied.
 
     Parameters
     ----------
@@ -90,57 +98,57 @@ def aggreate_categorical_features(df, features, agglo_feature="POSTCODE_UNIT"):
     features: list of strings
         Features of interest.
 
-    agglo_feature: str, default=POSTCODE_UNIT
-        Feature by which to agglomerate, for instance postcode level or hex id.
+    group_by_feature: str, default=POSTCODE_UNIT
+        Feature by which to aggregate, for instance postcode level or hex id.
 
 
     Return
     ---------
     df: pandas. Dataframe
-        Dataframe with agglomerated features."""
+        Dataframe with aggregated features."""
 
     aggregated_features = []
 
-    # Remove feature by which to agglomerate from features of interest
-    features = [f for f in features if f != agglo_feature]
+    # Remove feature by which to aggregate from features of interest
+    features = [f for f in features if f != group_by_feature]
 
-    # For each feature of interest, get agglomerated data
+    # For each feature of interest, get aggregated data
     for feature in features:
 
         # Group e.g. by postcode level
-        grouped_by_agglo_f = df.groupby(agglo_feature)
+        grouped_by_aggr_f = df.groupby(group_by_feature)
 
-        # Count the samples per agglomeration feature category
-        n_samples_agglo_cat = grouped_by_agglo_f[feature].count()
+        # Count the samples per aggregation feature category
+        n_samples_agglo_cat = grouped_by_aggr_f[feature].count()
 
-        # Get the feature categories by the agglomeration feature
-        feature_cats_by_agglo_f = (
-            df.groupby([agglo_feature, feature]).size().unstack(fill_value=0)
+        # Get the feature categories by the aggregation feature
+        feature_cats_by_aggr_f = (
+            df.groupby([group_by_feature, feature]).size().unstack(fill_value=0)
         )
 
         # Normalise by the total number of samples per category
-        cat_percentages_by_agglo_f = (feature_cats_by_agglo_f.T / n_samples_agglo_cat).T
+        cat_percentages_by_aggr_f = (feature_cats_by_aggr_f.T / n_samples_agglo_cat).T
 
         # Get the most frequent feature cateogry
-        cat_percentages_by_agglo_f[
+        cat_percentages_by_aggr_f[
             "MOST_FREQUENT_" + feature
-        ] = cat_percentages_by_agglo_f.idxmax(axis=1)
+        ] = cat_percentages_by_aggr_f.idxmax(axis=1)
 
         # Totals
-        cat_percentages_by_agglo_f[agglo_feature + "_TOTAL"] = n_samples_agglo_cat
+        cat_percentages_by_aggr_f[group_by_feature + "_TOTAL"] = n_samples_agglo_cat
 
         # Reset index
-        cat_percentages_by_agglo_f = cat_percentages_by_agglo_f.reset_index()
+        cat_percentages_by_aggr_f = cat_percentages_by_aggr_f.reset_index()
 
         # Rename columns with feature name + value
         col_rename_dict = {
             col: feature + ": " + str(col)
-            for col in cat_percentages_by_agglo_f.columns[1:-2]
+            for col in cat_percentages_by_aggr_f.columns[1:-2]
         }
-        cat_percentages_by_agglo_f.rename(columns=col_rename_dict, inplace=True)
+        cat_percentages_by_aggr_f.rename(columns=col_rename_dict, inplace=True)
 
         # Get the total
-        aggregated_features.append(cat_percentages_by_agglo_f)
+        aggregated_features.append(cat_percentages_by_aggr_f)
 
     # Concatenate and remove duplicates
     aggregated_features = pd.concat(aggregated_features, axis=1)
@@ -149,7 +157,7 @@ def aggreate_categorical_features(df, features, agglo_feature="POSTCODE_UNIT"):
     ]
 
     # Save number of samples per group (e.g. postcode level)
-    col = aggregated_features.pop(agglo_feature + "_TOTAL")
+    col = aggregated_features.pop(group_by_feature + "_TOTAL")
     aggregated_features.insert(1, col.name, col)
 
     return aggregated_features
@@ -180,28 +188,31 @@ def get_feature_count_grouped(df, feature, groupby_f, value=True, name=None):
 
     Return
     ---------
-    feature_cats_by_agglo_f: pandas.Dataframe
+    feature_cats_by_aggr_f: pandas.Dataframe
         Count of given feature value per group."""
 
     # Set default name
     if name is None:
-        name = feature + ": True"
+        name = feature + ": " + value
 
-    # Get the feature categories by the agglomeration feature
-    feature_cats_by_agglo_f = (
+    # Get the feature categories by the aggregation feature
+    feature_cats_by_aggr_f = (
         df.groupby([groupby_f, feature]).size().unstack(fill_value=0)
     ).reset_index()
 
     # Rename the column since "True" is not a meaningful name
-    feature_cats_by_agglo_f.rename(columns={value: name}, inplace=True)
+    feature_cats_by_aggr_f.rename(columns={value: name}, inplace=True)
 
-    return feature_cats_by_agglo_f[[groupby_f, name]]
+    return feature_cats_by_aggr_f[[groupby_f, name]]
 
 
-def encode_agglomerated_features(
+def encode_aggregated_features(
     df, postcode_level, ordinal_features, unaltered_features
 ):
-    """Encode the agglomerated features.
+    """Encode the aggregated features.
+    For numeric features, the median of all postcode level properties is computed.
+    For categorical feature, the percentage of category/value per postcode level is computed.
+    In addition, the most frequent category/value is detected and one-hot encoded.
 
     Parameters
     ----------
@@ -209,7 +220,7 @@ def encode_agglomerated_features(
         Dataframe with features to encode.
 
     postcode_level: str
-        Postcode level on which to agglomerate.
+        Postcode level on which to aggregate.
 
     ordinal_features: list
         List of ordinal features.
@@ -219,20 +230,19 @@ def encode_agglomerated_features(
 
     Return
     ---------
-    agglomerated_df : pandas.Dataframe
-        Encoded agglomerated features.
-    """
+    aggregated_df : pandas.Dataframe
+        Encoded aggregated features."""
 
     # Get numeric features
     numeric_features = df.select_dtypes(include=np.number).columns.tolist()
 
     # Agglomerate numeric features using median
-    num_agglomerated = (
-        df[numeric_features + [postcode_level]].groupby([postcode_level]).median()
+    num_aggregated = (
+        df[numeric_features + [postcode_level]].groupby(postcode_level).median()
     )
 
     # Reset index
-    num_agglomerated = num_agglomerated.reset_index()
+    num_aggregated = num_aggregated.reset_index()
 
     # Get categorical features (not ordinal, numeric or unaltered)
     categorical_features = [
@@ -244,29 +254,29 @@ def encode_agglomerated_features(
     ]
 
     # Aggreate categorical features
-    cat_agglomerated = aggreate_categorical_features(
-        df[categorical_features], categorical_features, agglo_feature=postcode_level
+    cat_aggregated = aggregate_categorical_features(
+        df[categorical_features], categorical_features, group_by_feature=postcode_level
     )
 
     # Get the features indicating the most frequent values
-    most_frequent = [col for col in cat_agglomerated.columns if "MOST_FREQUENT" in col]
+    most_frequent = [col for col in cat_aggregated.columns if "MOST_FREQUENT" in col]
 
-    # One-hot encode categorical features
-    cat_agglomerated = feature_encoding.one_hot_encoding(
-        cat_agglomerated, most_frequent, verbose=True
+    # One-hot encode categorical "most frequent" features
+    cat_aggregated = feature_encoding.one_hot_encoding(
+        cat_aggregated, most_frequent, verbose=True
     )
 
     # Concatenate the processed categorical, numeric and unaltered features
-    agglomerated_df = pd.concat(
+    aggregated_df = pd.concat(
         [
-            cat_agglomerated,
-            num_agglomerated.drop(columns=[postcode_level]),
+            cat_aggregated,
+            num_aggregated.drop(columns=[postcode_level]),
             df[unaltered_features],
         ],
         axis=1,
     )
 
-    return agglomerated_df
+    return aggregated_df
 
 
 def get_target_variables(
@@ -281,10 +291,10 @@ def get_target_variables(
         Dataframe with all year's data, used for normalising.
 
     source_year: pandas.Dataframe
-        Dataframe with data for source year.
+        Dataframe with data up to source year.
 
     target_year: pandas.Dataframe
-        Dataframe with data for target year.
+        Dataframe with data up to target year.
 
     postcode_level: str
         Postcode level to group by.
@@ -292,7 +302,8 @@ def get_target_variables(
     normalise: str, default='total'
         How to normalise the number of properties per postcode.
         'total' : normalise by total number of properties
-        'target' : normalise by number of properties at target year
+        'target' : normalise by number of properties by target year
+        'total HPs' : normalise by total number of HPs
 
     Return
     ---------
@@ -301,10 +312,10 @@ def get_target_variables(
 
     # Get the number of installed heat pumps per group (e.g. postcode level)
     n_hp_installed_source = get_feature_count_grouped(
-        source_year, "HP_INSTALLED", postcode_level, name="# HP at Time t"
+        source_year, "HP_INSTALLED", postcode_level, name="# HP by source year"
     )
     n_hp_installed_target = get_feature_count_grouped(
-        target_year, "HP_INSTALLED", postcode_level, name="# HP at Time t+1"
+        target_year, "HP_INSTALLED", postcode_level, name="# HP by target year"
     )
     n_hp_installed_total = get_feature_count_grouped(
         df, "HP_INSTALLED", postcode_level, name="Total # HP"
@@ -315,6 +326,8 @@ def get_target_variables(
         total_basis = df
     elif normalise == "target":
         total_basis = target_year
+    elif normalise == "total HPs":
+        total_basis = n_hp_installed_total
     else:
         raise IOError(
             "Normalisation type '{}' is not defined. Please use 'total' or 'target' instead.".format(
@@ -337,10 +350,10 @@ def get_target_variables(
 
     # Compute heat pump coverage and growth
     target_var_df["HP_COVERAGE_CURRENT"] = (
-        target_var_df["# HP at Time t"] / target_var_df["# Properties"]
+        target_var_df["# HP by source year"] / target_var_df["# Properties"]
     )
     target_var_df["HP_COVERAGE_FUTURE"] = (
-        target_var_df["# HP at Time t+1"] / target_var_df["# Properties"]
+        target_var_df["# HP by target year"] / target_var_df["# Properties"]
     )
     target_var_df["GROWTH"] = (
         target_var_df["HP_COVERAGE_FUTURE"] - target_var_df["HP_COVERAGE_CURRENT"]
